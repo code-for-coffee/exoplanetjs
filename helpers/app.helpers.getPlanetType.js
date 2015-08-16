b5f9209a284cf8b6ba6b5c996300af3bdccd1068
James Traver <jtraverjr@gmail.com>
2015-08-16 00:13:03 -0500

    helpers - getPlanetType

---
+++ helpers/app.helpers.getPlanetType.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+app.helpers = app.helpers || {};
+
+/**
+ *  @earthMass: Float value for a planet's relative Earth Mass
+ *  return String of Official Planet Type
+ */
+app.helpers.getPlanetType = function(earthMass) {
+
+  var planetType;
+
+  return planetType;
+
+};
