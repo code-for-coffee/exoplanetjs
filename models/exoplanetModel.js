d55506ab461780f8ef61b32756affa8f94d9aa53
James <code-for-coffee@users.noreply.github.com>
2015-08-16 14:42:06 -0500

    Update exoplanetModel.js

--- models/exoplanetModel.js
+++ models/exoplanetModel.js
@@ -6,27 +6,7 @@
  * @param paramsObject
  * @return
  */
-class modelClass {
-  
-  let reservedWords = ['mapValues', 'properties', 'toObject', 'attributes'];
-  
-  let keyList = Object.keys(paramsObject);
-    keyList.forEach(function(key) {
 
-      var attributeName = key;
-  
-      reservedWords.forEach(function(key) {
-         if (attributeName == key) throw new ReferenceError('Illegal use of reservedword in parameters:' + attributeName);
-      });
-
-  });
-  
-  toObject() {
-    return this.properties;
-  }
-  
-}
- 
 class exoplanetModel extends modelClass {
 
   constructor(properties) {
