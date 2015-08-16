544a9a1a9d2d55d9a93013407f8a9ec9d2347068
James Traver <jtraverjr@gmail.com>
2015-08-16 00:16:01 -0500

    correcting mis-spelled name

--- conversions/app.conversions.parseJupiterMassFromNASAPlanetData.js
+++ conversions/app.conversions.parseJupiterMassFromNASAPlanetData.js
@@ -2,7 +2,7 @@ var app = app || {};
 
 app.conversions = app.conversions || {};
 
-app.conversions.parseMassFromNASAPlanetData = function(exoplanetObject) {
+app.conversions.parseJupiterMassFromNASAPlanetData = function(exoplanetObject) {
   if (!typeof exoplanet == 'object') return false;
 
   var exoplanetMass;
