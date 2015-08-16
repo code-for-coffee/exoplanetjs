937f0c26a34d119696ad73dcc203e6094aae285b
James <code-for-coffee@users.noreply.github.com>
2015-08-16 14:42:26 -0500

    added modelClass

---
+++ models/modelClass.js
@@ -0,0 +1,20 @@
+class modelClass {
+  
+  let reservedWords = ['mapValues', 'properties', 'toObject', 'attributes'];
+  
+  let keyList = Object.keys(paramsObject);
+    keyList.forEach(function(key) {
+
+      var attributeName = key;
+  
+      reservedWords.forEach(function(key) {
+         if (attributeName == key) throw new ReferenceError('Illegal use of reservedword in parameters:' + attributeName);
+      });
+
+  });
+  
+  toObject() {
+    return this.properties;
+  }
+  
+}
